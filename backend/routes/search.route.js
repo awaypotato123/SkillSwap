f7103b79df761d6e6efaa3fe3ad32ae07c1c290a
christian <christiankhan100@gmail.com>
2025-11-13 00:15:21 -0500

    fix import in search route

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { search } from "../controllers/search.controller";
+import { search } from "../controllers/search.controller.js";
 
 const router = Router();
 
